01e62b887e6f4432abb109f86ac36ba95465d637
branca della coletta <brancasdc@gmail.com>
2023-11-07 10:44:43 -0300

    criado controller de pages, criado método home e adicionado como rota principal (root), criada tabela de disponibilidade de horários no html de home

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def home
+    @bookings = Booking.all
+    current_date = params[:week] ? Date.parse(params[:week]) : Date.today
+    @first_day_of_week = current_date.beginning_of_week(:monday)
+    @last_day_of_week = current_date.end_of_week(:saturday)
+    @dates_for_days = (0..4).map { |day| @first_day_of_week + day }
+  end
+end
