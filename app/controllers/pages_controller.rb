9d701e1d8f23786f921b49c538f081800fe758e8
branca della coletta <brancasdc@gmail.com>
2023-11-07 18:17:53 -0300

    corrigida condição de data para criar/editar/excluir reserva

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @current_date = params[:week] ? Date.parse(params[:week]) : Date.today
-    @first_day_of_week = @current_date.beginning_of_week(:monday)
-    @last_day_of_week = @current_date.end_of_week(:saturday)
+    current_date = params[:week] ? Date.parse(params[:week]) : Date.today
+    @first_day_of_week = current_date.beginning_of_week(:monday)
+    @last_day_of_week = current_date.end_of_week(:saturday)
     @dates_for_days = (0..4).map { |day| @first_day_of_week + day }
   end
 end
