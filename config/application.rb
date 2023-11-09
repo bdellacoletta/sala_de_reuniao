b662a1408da7dc76e4d2cdbfe1c3a3809ebf23a7
branca della coletta <brancasdc@gmail.com>
2023-11-09 12:45:09 -0300

    adicionado configuração para precompile

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module SalaDeReuniao
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :pt
+    config.assets.precompile += %w( *.css *.js )
   end
 end
