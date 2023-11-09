8dd9fca94d280700630b5b1628d8d92acbd4cb7c
branca della coletta <brancasdc@gmail.com>
2023-11-09 12:22:31 -0300

    corrigadas rotas e corrigida lógica de edição/delete de reserva no frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :bookings, only: %i[new create edit update destroy]
+  resources :bookings, except: %i[index show]
 end
